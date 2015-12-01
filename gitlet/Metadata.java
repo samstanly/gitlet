441d81e046b31344949136eb6f05d1e747f44d99
Jamie Ni <jamieni@berkeley.edu>
2015-11-30 17:01:51 -0800

    add files

---
+++ gitlet/Metadata.java
@@ -0,0 +1,24 @@
+package gitlet;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.Serializable;
+import java.util.Date;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import ucb.util.CommandArgs;
+
+public class Metadata implements Serializable {
+	protected String HEAD;
+  static Commit getHead() {
+  	
+  }
+}
